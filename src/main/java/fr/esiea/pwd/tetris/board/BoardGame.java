fc71b46128745d54bb8d0611cf9a4604c067e60c
Gauthier <Gauthier@192.168.1.86>
2016-03-30 16:11:01 +0200

    Ajout de boardgame

--- src/main/java/fr/esiea/pwd/tetris/board/BoardGame.java
+++ src/main/java/fr/esiea/pwd/tetris/board/BoardGame.java
@@ -1,5 +1,18 @@
 package fr.esiea.pwd.tetris.board;
 
-public class BoardGame {
+import java.io.IOException;
+import java.util.*;
 
+import fr.esiea.pwd.tetris.movement.Movement;
+
+public class BoardGame implements printable {
+	private boolean loose = false;
+	private MovingPiece piece;
+	MovementValidator movementValidator;
+	
+	public void applyMovements(List<Movement> movements){
+		for (Movement : movement : movements){
+			movement.move(movementValidator, piece);
+		}
+	}
 }
