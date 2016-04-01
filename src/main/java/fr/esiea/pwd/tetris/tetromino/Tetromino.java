790dd4d0debb1ecd9ab16fbd551dc49fc336433e
Gauthier <Gauthier@192.168.1.86>
2016-04-01 16:33:41 +0200

    Finition Tetromino

--- src/main/java/fr/esiea/pwd/tetris/tetromino/Tetromino.java
+++ src/main/java/fr/esiea/pwd/tetris/tetromino/Tetromino.java
@@ -1,31 +1,25 @@
 package fr.esiea.pwd.tetris.tetromino;
 import java.util.*;
-public enum Tetromino {
+public enum Tetromino{
+	SQUARE(new int[][] {{0,4},{0,5},{1,4},{1,5}}),
+	T(new int[][] {{0,3},{0,4},{0,5},{1,4}}),
+	L(new int[][] {{0,4},{1,4},{2,4},{2,5}}),
+	LINV(new int[][] {{0,5},{1,5},{2,5},{2,4}}),
+	LINE(new int[][] {{0,3},{0,4},{0,5},{0,6}}),
+	S(new int[][] {{0,6},{0,5},{1,5},{1,4}}),
+	Z(new int[][] {{0,4},{0,5},{1,5},{1,6}});
 	
-	SQUARE(0,1,0), 
-	T,
-	L,
-	LINV,
-	LINE,
-	S,
-	Z;
+	private int[][] position;
 	
-	private int width = 4;
-	private int height = 4;
-	private boolean box[][] = new boolean[height][width];
-	
-	public static void clearArray(boolean[] box){
-		for(boolean[] row: box)
-			Arrays.fill(row,false);
+	Tetromino(int[][] position){
+		this.position = position;
 	}
 	
-	SQUARE {
-		
-		
+	private static final List<Tetromino> VALUES = Collections.unmodifiableList(Arrays.asList(values()));
+	private static final int SIZE = VALUES.size();
+	private static final Random RANDOM = new Random();
 
-		
+	public static Tetromino randomTetro()  {
+		return VALUES.get(RANDOM.nextInt(SIZE));
 	}
-	
-
-
 }
