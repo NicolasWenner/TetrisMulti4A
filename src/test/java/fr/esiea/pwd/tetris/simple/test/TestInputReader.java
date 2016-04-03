893941fb79d296fa10db27db4a4fa4158614063a
bliss pelete <pelete@et.esiea.fr>
2016-04-03 19:31:58 +0200

    test input reader

--- src/test/java/fr/esiea/pwd/tetris/simple/test/TestInputReader.java
+++ src/test/java/fr/esiea/pwd/tetris/simple/test/TestInputReader.java
@@ -1,5 +1,18 @@
 package fr.esiea.pwd.tetris.simple.test;
+import static org.mockito.Mockito.*;
+
+import org.mockito.Mockito;
+
+import fr.esiea.pwd.tetris.inputreader.InputReader;
+
 
 public class TestInputReader {
+	
+	public void testInput(char input) {
+	
+	InputReader reader = Mockito.mock(InputReader.class);
+	
 
+
+}
 }
